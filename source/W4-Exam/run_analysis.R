5877bf6eec2b46ffd28406735026e79e3a2dec43
Gabriele Roggero <gabriele.roggero@gmail.com>
2019-07-11 00:02:07 +0200

    Upload W4 Submission's files

--- source/W4-Exam/run_analysis.R
+++ source/W4-Exam/run_analysis.R
@@ -9,12 +9,16 @@
 Packages <- c("dplyr", "reshape2")
 lapply(Packages, library, character.only = TRUE)
 
-setwd("C:/Users/Gabriele/Documents/Git/JHSPH-Data-Science/data/W4-Exam/UCI HAR Dataset")
+# Download and unzip of files
+path <- getwd()
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url, file.path(path, "dataFiles.zip"))
+unzip(zipfile = "dataFiles.zip")
 
 # Activity_labels.txt: Links the class labels with their activity name.
-ActivityLabels <- read.table("activity_labels.txt", header = FALSE, sep = "", dec = ".", col.names = c("ClassLabels", "ActivityName"))
+ActivityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE, sep = "", dec = ".", col.names = c("ClassLabels", "ActivityName"))
 # Features.txt: List of all features.
-Features <- read.table("features.txt", header = FALSE, sep = "", dec = ".", col.names = c("ID", "FeatureName"))
+Features <- read.table("UCI HAR Dataset/features.txt", header = FALSE, sep = "", dec = ".", col.names = c("ID", "FeatureName"))
 # Subsetting all the ID satisfying condition 2.
 FeaturesFiltered <- grep("(mean|std)\\(\\)", Features[, "FeatureName"])
 # selecting the only rows satisfying condition 2.
@@ -24,7 +28,7 @@ ValidMeasures <- gsub('[()]', '', ValidMeasures)
 
 ### X TRAIN ###
 # Loading training input file eand renaming columns.
-ds_train_X <- setNames(read.table("train/X_train.txt", header = FALSE, sep = "", dec = "."), 1:561)
+ds_train_X <- setNames(read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "", dec = "."), 1:561)
 # Selecting the only columns by filtered features.
 ds_train_X <- ds_train_X %>% select(FeaturesFiltered)
 # Renaming columns.
@@ -32,24 +36,24 @@ ds_train_X <- setNames(ds_train_X, ValidMeasures)
 
 ### Y TRAIN ###
 # Loading train activities input file and renaming columns.
-TrainActivities <- read.table("train/Y_train.txt", header = FALSE, sep = "", dec = ".", col.names = c("Activity"))
+TrainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt", header = FALSE, sep = "", dec = ".", col.names = c("Activity"))
 # Loading subject input file who performed activities.
-TrainSubjects <- read.table("train/subject_train.txt", header = FALSE, sep = "", dec = ".", col.names = c("IDSubject"))
+TrainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE, sep = "", dec = ".", col.names = c("IDSubject"))
 # Putting all these datasets togheter.
 ds_Train <- cbind(TrainSubjects, TrainActivities, ds_train_X)
 
 
 ### X TEST ###
 # Loading testing input file eand renaming columns.
-ds_test_X <- setNames(read.table("test/X_test.txt", header = FALSE, sep = "", dec = "."), 1:561)
+ds_test_X <- setNames(read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE, sep = "", dec = "."), 1:561)
 # Selecting the only columns by filtered features.
 ds_test_X <- ds_test_X %>% select(FeaturesFiltered)
 # Renaming columns.
 ds_test_X <- setNames(ds_test_X, ValidMeasures)
 
 ### Y TEST ###
-TestActivities <- read.table("test/Y_test.txt", header = FALSE, sep = "", dec = ".", col.names = c("Activity"))
-TestSubjects <- read.table("test/subject_test.txt", header = FALSE, sep = "", dec = ".", col.names = c("IDSubject"))
+TestActivities <- read.table("UCI HAR Dataset/test/Y_test.txt", header = FALSE, sep = "", dec = ".", col.names = c("Activity"))
+TestSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE, sep = "", dec = ".", col.names = c("IDSubject"))
 ds_Test <- cbind(TestSubjects, TestActivities, ds_test_X)
 
 # Appending all datasets.
